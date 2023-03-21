0c190e09e3916e4900f583852e4dd892e45f0df2
Kevin <ksider.ledantec@gmail.com>
2023-03-21 22:06:04 +0100

    Commit 21/03

--- PARTIE-ETUDIANT/front_end/questions.php
+++ PARTIE-ETUDIANT/front_end/questions.php
@@ -4,6 +4,7 @@
     <title>Questions | PsychoQuizz</title>
     <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/main.css">
     <link rel="stylesheet" type="image/png" href="../assets/image/fld.PNG">
+    <link rel="icon" type="image/x-icon" href="../assets/image/fld.PNG">
 </head>
 <header>
       <img src="../assets/image/fld.png" alt="https://imageshack.com/i/pmkGydn7p" alt="fld">
@@ -12,46 +13,39 @@
 <body>
     <div class="container-questions">
 
-    <?php      //Connexion à la bdd
-
+    <?php
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "quizz";
-
+    
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
-        echo "<p>Erreur.</p>";           //alternative mysqli erreur pdo
+        echo "<p>Une erreur est survenue : " . $e->getMessage() . "</p>";
         die();
     }
-
+    
     // Récupération de la question courante
     $current_question = isset($_GET['id']) ? (int)$_GET['id'] : 0;
-
-    $sql = "SELECT MAX(IDQUESTION) AS max_id, IDQUESTION, LIBELLE, IDTYPEQUESTION FROM question WHERE IDQUESTION > $current_question ORDER BY IDQUESTION LIMIT 1";
-    $result = $conn->query($sql);
-
-    if ($result === false) {
-        echo "<p>Erreur</p>";
-        die();
-    }
-
-    if ($result->rowCount() > 0) {
-        $row = $result->fetch(PDO::FETCH_ASSOC);
+    
+    $sql = "SELECT MAX(IDQUESTION) AS max_id, IDQUESTION, LIBELLE, IDTYPEQUESTION FROM question WHERE IDQUESTION > ? ORDER BY IDQUESTION LIMIT 1";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$current_question]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if (!$row) {
+        $id_question = 0;
+        $max_id = 0;
+        $libelle_question = "Il n'y a pas de question suivante.";
+    } else {
         $id_question = $row["IDQUESTION"];
         $libelle_question = $row["LIBELLE"];
         $max_id = $row["max_id"];
         $idtest = $row["IDTYPEQUESTION"];
-        
-    } else {
-        $id_question = 0;
-        $max_id = 0;
-        $libelle_question = "Il n'y a pas de question suivante.";
     }
-
+    
     $conn = null;
 
     if ($idtest == 1) {
@@ -60,31 +54,26 @@
         $texte .= '<a href="?id=' . $id_question . '" class="btnNon">Non</a>';
     } elseif ($idtest == 2) {
         $texte = "<h2>" . $id_question . " - " . $libelle_question . " ? </h2>";
-    $texte .= '<form class="rdb-group2">';
-    $texte .= '<label class="rdbRouge"><input type="radio" name="color" value="purple"> Pas du tout</label>';
-    $texte .= '<label class="rdbOrange"><input type="radio" name="color" value="yellow"> Un peu</label>';
-    $texte .= '<label class="rdbJaune"><input type="radio" name="color" value="green"> Modérément</label>';
-    $texte .= '<label class="rdbJauneVert"><input type="radio" name="color" value="blue"> Beaucoup</label>';
-    $texte .= '<label class="rdbVert"><input type="radio" name="color" value="red"> Énormément</label>';
-    $texte .= '</form>';
-    } else {
-        $texte = "IDTYPEQUESTION non valide.";
+        $texte .= '<form class="rdb-group2">';
+        $texte .= '<label class="rdbRouge"><input type="radio" name="color" value="purple"> Pas du tout</label>';
+        $texte .= '<label class="rdbOrange"><input type="radio" name="color" value="yellow"> Un peu</label>';
+        $texte .= '<label class="rdbJaune"><input type="radio" name="color" value="green"> Modérément</label>';
+        $texte .= '<label class="rdbJauneVert"><input type="radio" name="color" value="blue"> Beaucoup</label>';
+        $texte .= '<label class="rdbVert"><input type="radio" name="color" value="red"> Énormément</label>';
+        $texte .= '</form>';
     }
-?>
+    ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Programme PHP/HTML</title>
 </head>
 <body>
-	<h1>Texte en fonction de IDTYPEQUESTION</h1>
+	<h1>Affichage des questions</h1>
 	<p><?php echo $texte; ?></p>
-</div>
-    <button class="btn-container">
-        <a class="btnSuivant" href="?Id">Suivant  [<?php echo $id_question; ?> / <?php echo $max_id; ?>]</a>
-    </button>
-    <footer></footer>
+    </div>
+  <a href="?id=<?php echo $id_question ?>" class="btnSuivant">Suivant <?php echo $id_question ?> / <?php echo $max_id ?></a>
 </body>
+<footer></footer>
 </html>
